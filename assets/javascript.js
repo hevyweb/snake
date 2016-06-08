12e9c88c9e067c82c919b756f53fcc9ff2ca06e3
Dmytro Dzyuba <1932@bk.ru>
2016-06-08 18:12:03 +0300

    Snake can get additional rings.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     Snake = function(){
         return {
+            snake: [],
+
+            stage: null,
+
+            ring: null,
+
+            configs: {
+                width: 20,
+                height: 20
+            },
+
             init: function(){
                 $('body').html('');
                 this.buildStage()
@@ -25,8 +36,81 @@ $(document).ready(function(){
                     })
                 });
             },
+
             startTheGame: function(){
-                alert('Under construction');
+                this.addRing(3);
+            },
+
+            addRing: function(n){
+                var ring;
+                if (this.ring == null){
+                    this.ring = $('<div class="ring"></div>');
+                }
+                for (;n>0;n--) {
+                    ring = this.ring.clone();
+                    this.stage.append(ring);
+                    this.reorder(ring);
+                    this.snake.push(ring);
+                }
+            },
+
+            reorder: function(ring){
+                var x,y;
+                switch (this.detectDirection()) {
+                    case 'up':
+                        x = 0;
+                        y = -1*this.configs.height;
+                        break;
+                    case 'left':
+                        x = -1 * this.configs.width;
+                        y = 0;
+                        break;
+                    case 'right':
+                        x = this.configs.width;
+                        y = 0;
+                        break;
+                    default:
+                        x = 0;
+                        y = this.configs.height;
+                }
+                var snakeLength = this.snake.length;
+                if (snakeLength) {
+                    var lastRing = this.snake[snakeLength-1].position;
+                } else {
+                    var lastRing = {
+                        left: parseInt((this.stage.width() - this.configs.width*2)/2),
+                        top: parseInt((this.stage.height() - this.configs.height*2)/2)
+                    };
+                }
+
+                ring.css({
+                    left: lastRing.left+x,
+                    top: lastRing.top+y
+                });
+
+            },
+
+            detectDirection: function(){
+                var snakeLength = this.snake.length;
+                var direction = 'down';
+                if (snakeLength) {
+                    var lastRing = this.snake[snakeLength-1].position();
+                    if (snakeLength>1){
+                        var preLastRing = this.snake[snakeLength-2].position();
+                        if (lastRing.left == preLastRing.left){
+                            if (lastRing.top<preLastRing.top){
+                                direction = 'up';
+                            }
+                        } else {
+                            if (lastRing.left < preLastRing.left){
+                                direction = 'right';
+                            } else {
+                                direction = 'left';
+                            }
+                        }
+                    }
+                }
+                return direction;
             }
         }
     };
