180acf5936c72a920c814c65479c343b836448df
Dmytro_Dzyuba <1932@bk.ru>
2016-06-29 00:33:21 +0300

    Fixed some issues.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,54 +1,34 @@
-$(document).ready(function(){
-    Snake = function(){
+$(document).ready(function() {
+    Snake = function() {
         return {
             snake: [],
-
             stage: null,
-
-            ring: null,
-            
+            ring: $('<div class="ring"></div>'),
             engine: null,
-            
             speed: 500,
-            
             maxSpeed: 50,
-            
             speedStep: null,
-            
             dotPosition: null,
-
             configs: {
                 width: 20,
-                height: 20,
-                sector: {
-                    colls: 4,
-                    rows: 3,
-                    width: null,
-                    height: null
-                }
+                height: 20
             },
-            
             direction: {
                 'top': -20,
                 'left': 0
             },
-            
             lastKeyCode: 38,
-            
             lastSector: 0,
-
-            init: function(){
-                
+            init: function() {
                 $('body').html('');
                 $(window).keyup(this.setEventListeners(this));
                 this.buildStage();
-                this.speedStep = parseInt(this.configs.sector.horizontalCells*this.configs.sector.verticalCells/Math.PI);
+                this.speedStep = parseInt(this.configs.sector.horizontalCells * this.configs.sector.verticalCells / Math.PI);
             },
-
-            setEventListeners: function (self){
-                return function(e){
+            setEventListeners: function(self) {
+                return function(e) {
                     var top, left;
-                    switch(e.keyCode){
+                    switch (e.keyCode) {
                         case 38:
                             if (self.lastKeyCode !== 38 && self.lastKeyCode !== 40) {
                                 top = -1 * self.configs.height;
@@ -75,8 +55,8 @@ $(document).ready(function(){
                             break;
                     }
                     self.lastKeyCode = e.keyCode;
-                    
-                    if (top || left){
+
+                    if (top || left) {
                         self.direction = {
                             top: top,
                             left: left
@@ -85,63 +65,60 @@ $(document).ready(function(){
                     }
                 };
             },
-            
-            buildStage: function (){
-                var height = parseInt($(window).height()/2);
-                var width = parseInt($(window).width()/2);
+            buildStage: function() {
+                var height = parseInt($(window).height() / 2);
+                var width = parseInt($(window).width() / 2);
                 this.stage = $('<div class="stage" />').css(this.resizeStage(height, width)).appendTo('body');
                 var self = this;
-                $('<div class="startBtn">Go!</div>').appendTo(this.stage).click(function(){
+                $('<div class="startBtn">Go!</div>').appendTo(this.stage).click(function() {
                     $(this).animate({
                         opacity: 0,
                         height: 0,
                         width: 0,
                         "line-height": 0,
                         "font-size": 0
-                    }, 400, function(){
+                    }, 400, function() {
                         $(this).remove();
                         self.startTheGame();
                     });
                 });
             },
-            
             resizeStage: function(height, width) {
-                this.configs.sector.width = Math.floor(Math.floor(width/this.configs.width)/this.configs.sector.colls)*this.configs.width;
-                this.configs.sector.height = Math.floor(Math.floor(height/this.configs.height)/this.configs.sector.rows)*this.configs.height;
-                this.configs.sector.horizontalCells = this.configs.sector.width/this.configs.width;
-                this.configs.sector.verticalCells = this.configs.sector.height/this.configs.height;
+                var horizontalCells = Math.floor(width / this.configs.width);
+                var verticalCells = Math.floor(height / this.configs.height);
+
+                this.totalCells = horizontalCells * verticalCells;
 
                 return {
-                    'width': this.configs.sector.width * this.configs.sector.colls,
-                    'height': this.configs.sector.height * this.configs.sector.rows
+                    'width': verticalCells * this.configs.width,
+                    'height': horizontalCells * this.configs.width
                 };
             },
-
-            startTheGame: function(){
+            startTheGame: function() {
                 this.addRing(4);
                 var self = this;
-                this.engine = setInterval(function(){self.run();}, this.speed);
-                setTimeout(function(){self.appearDot();}, 500);
+                this.engine = setInterval(function() {
+                    self.run();
+                }, this.speed);
+                setTimeout(function() {
+                    self.appearDot();
+                }, 500);
             },
-            
-            stopTheGame: function(){
+            stopTheGame: function() {
                 clearInterval(this.engine);
                 alert('You loose');
             },
-            
-            restartTheEngine: function(){
+            restartTheEngine: function() {
                 clearInterval(this.engine);
                 var self = this;
                 self.run();
-                this.engine = setInterval(function(){self.run();}, this.speed);
+                this.engine = setInterval(function() {
+                    self.run();
+                }, this.speed);
             },
-
-            addRing: function(n){
+            addRing: function(n) {
                 var ring;
-                if (this.ring === null){
-                    this.ring = $('<div class="ring"></div>');
-                }
-                for (;n>0;n--) {
+                for (; n > 0; n--) {
                     ring = this.ring.clone();
                     if (!this.snake.length) {
                         ring.addClass('head');
@@ -151,13 +128,12 @@ $(document).ready(function(){
                     this.snake.push(ring);
                 }
             },
-
-            reorder: function(ring){
+            reorder: function(ring) {
                 var x, y, lastRing;
                 switch (this.detectDirection()) {
                     case 'up':
                         x = 0;
-                        y = -1*this.configs.height;
+                        y = -1 * this.configs.height;
                         break;
                     case 'left':
                         x = -1 * this.configs.width;
@@ -173,34 +149,33 @@ $(document).ready(function(){
                 }
                 var snakeLength = this.snake.length;
                 if (snakeLength) {
-                    lastRing = this.snake[snakeLength-1].position();
+                    lastRing = this.snake[snakeLength - 1].position();
                 } else {
                     lastRing = {
-                        left: Math.floor(this.configs.sector.horizontalCells*this.configs.sector.colls/2)*this.configs.width,
-                        top: Math.floor(this.configs.sector.verticalCells*this.configs.sector.rows/2)*this.configs.height
+                        left: Math.floor(this.configs.sector.horizontalCells * this.configs.sector.colls / 2) * this.configs.width,
+                        top: Math.floor(this.configs.sector.verticalCells * this.configs.sector.rows / 2) * this.configs.height
                     };
                 }
 
                 ring.css({
-                    left: lastRing.left+x,
-                    top: lastRing.top+y
+                    left: lastRing.left + x,
+                    top: lastRing.top + y
                 });
 
             },
-
-            detectDirection: function(){
+            detectDirection: function() {
                 var snakeLength = this.snake.length;
                 var direction = 'down';
                 if (snakeLength) {
-                    var lastRing = this.snake[snakeLength-1].position();
-                    if (snakeLength>1){
-                        var preLastRing = this.snake[snakeLength-2].position();
-                        if (lastRing.left === preLastRing.left){
-                            if (lastRing.top<preLastRing.top){
+                    var lastRing = this.snake[snakeLength - 1].position();
+                    if (snakeLength > 1) {
+                        var preLastRing = this.snake[snakeLength - 2].position();
+                        if (lastRing.left === preLastRing.left) {
+                            if (lastRing.top < preLastRing.top) {
                                 direction = 'up';
                             }
                         } else {
-                            if (lastRing.left < preLastRing.left){
+                            if (lastRing.left < preLastRing.left) {
                                 direction = 'right';
                             } else {
                                 direction = 'left';
@@ -210,24 +185,23 @@ $(document).ready(function(){
                 }
                 return direction;
             },
-            
-            run: function(){
+            run: function() {
                 var ring, ringPosition, lastPosition, newPosition;
-                for(var n = 0; n<this.snake.length; n++)
+                for (var n = 0; n < this.snake.length; n++)
                 {
                     ring = this.snake[n];
                     ringPosition = ring.position();
-                    if (n){
+                    if (n) {
                         newPosition = lastPosition;
                     } else {
                         newPosition = {
-                            'left' : ringPosition.left + this.direction.left,
-                            'top' : ringPosition.top + this.direction.top
+                            'left': ringPosition.left + this.direction.left,
+                            'top': ringPosition.top + this.direction.top
                         };
-                        if (this.checkValidPosition(newPosition)){
+                        if (this.checkValidPosition(newPosition)) {
                             return this.stopTheGame();
                         }
-                
+
                         this.eatDot(newPosition);
                     }
 
@@ -235,17 +209,16 @@ $(document).ready(function(){
                     lastPosition = ringPosition;
                 }
             },
-            
-            checkValidPosition: function(newPosition){
+            checkValidPosition: function(newPosition) {
                 if (
-                    newPosition.left<0 || 
-                    newPosition.top<0 || 
-                    newPosition.top+this.configs.height > this.stage.height() || 
-                    newPosition.left+this.configs.width > this.stage.width()
-                ){
+                        newPosition.left < 0 ||
+                        newPosition.top < 0 ||
+                        newPosition.top + this.configs.height > this.stage.height() ||
+                        newPosition.left + this.configs.width > this.stage.width()
+                        ) {
                     return true;
                 } else {
-                    for(var l = this.snake.length-1;l>0;l--){
+                    for (var l = this.snake.length - 1; l > 0; l--) {
                         movingRing = this.snake[l].position();
                         if (newPosition.left === movingRing.left && newPosition.top === movingRing.top) {
                             return true;
@@ -254,39 +227,47 @@ $(document).ready(function(){
                 }
                 return false;
             },
-            
-            appearDot: function(){
-                var sector = this.lastSector;
+            appearDot: function() {
+                console.log("started appearDot");
+                var sector = this.lastSector,
+                        wrongPosition = false,
+                        loop = 0;
                 do {
-                    var wrongPosition = false;
-                    while(sector === this.lastSector) {
+                    console.log('doloop');
+                    wrongPosition = false;
+                    loop = 0;
+                    do {
+                        console.log('whileloop');
                         sector = Math.ceil(Math.random() * (this.configs.sector.rows * this.configs.sector.colls - 1));
-                    }
-                    var row = Math.floor(sector/this.configs.sector.colls);
-                    var coll = sector%this.configs.sector.colls;
-                    var top = row * this.configs.sector.height + Math.floor(Math.random() * this.configs.sector.verticalCells)*this.configs.height;
-                    var left = coll * this.configs.sector.width + Math.floor(Math.random() * this.configs.sector.horizontalCells)*this.configs.width;
-                    for (var l = this.snake.length - 1; l >= 0; l--){
+                        if (loop > 10) {
+                            break;
+                        }
+                        loop++;
+                    } while (sector === this.lastSector);
+                    var row = Math.floor(sector / this.configs.sector.colls);
+                    var coll = sector % this.configs.sector.colls;
+                    var top = row * this.configs.sector.height + Math.floor(Math.random() * this.configs.sector.verticalCells) * this.configs.height;
+                    var left = coll * this.configs.sector.width + Math.floor(Math.random() * this.configs.sector.horizontalCells) * this.configs.width;
+                    for (var l = this.snake.length - 1; l >= 0; l--) {
+                        console.log('forloop');
                         var position = $(this.snake[l]).position();
                         if (position.top === top || position.left === left) {
                             wrongPosition = true;
                         }
                     }
-                } while(wrongPosition);
-                
-                return $('<div class="ring dot"></div>')
-                    .css({
-                        'top': top,
-                        'left': left
-                    })
-                    .appendTo(this.stage);
+                } while (wrongPosition);
+                console.log('completed appearDot');
+                return this.ring.clone().addClass('dot')
+                        .css({
+                            'top': top,
+                            'left': left
+                        })
+                        .appendTo(this.stage);
             },
-            
-            moveToTheTail: function(){
-                
+            moveToTheTail: function() {
+
             },
-            
-            eatDot: function(headPosition){
+            eatDot: function(headPosition) {
                 var dot = $('.dot');
                 if (!dot.length) {
                     return;
@@ -295,17 +276,19 @@ $(document).ready(function(){
                     this.dotPosition = dot.position();
                 }
 
-                if (this.dotPosition.top === headPosition.top && this.dotPosition.left === headPosition.left){
+                if (this.dotPosition.top === headPosition.top && this.dotPosition.left === headPosition.left) {
                     this.dotPosition = null;
-                    dot.removeClass('dot');
-                    this.snake.push(dot);
-                    this.appearDot();
-                    
+                    dot.remove();
+                    this.addRing(1);
+                    /*dot.removeClass('dot');
+                     this.snake.push(dot);
+                     */this.appearDot();
+
                     if (this.speed - this.speedStep >= this.maxSpeed) {
                         this.speed -= this.speedStep;
                     }
                     console.log(this.speed);
-                }                
+                }
             }
         };
     };
